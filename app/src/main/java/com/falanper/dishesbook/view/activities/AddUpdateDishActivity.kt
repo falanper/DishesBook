6112a014af3a0afbd5444b400b8098a56ba626e2
Fabio Pereira <fpereira5511@gmail.com>
2023-06-30 11:25:46 +0200

    added camera access

--- app/src/main/java/com/falanper/dishesbook/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/falanper/dishesbook/view/activities/AddUpdateDishActivity.kt
@@ -1,13 +1,35 @@
 package com.falanper.dishesbook.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.falanper.dishesbook.R
 import com.falanper.dishesbook.databinding.ActivityAddUpdateDishBinding
+import com.falanper.dishesbook.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val CAMERA = 1
+    }
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
@@ -22,9 +44,7 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private fun setUpActionBar() {
         mBinding.toolbarAddDishesActivity.apply {
             setSupportActionBar(this)
-            setNavigationOnClickListener {
-                onBackPressedDispatcher.onBackPressed()
-            }
+            setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
@@ -32,9 +52,119 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.ivAddDishImage -> {
-                Toast.makeText(this, "Photo image clicked!", Toast.LENGTH_SHORT).show()
+                customImageSelectionDialog()
                 return
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.apply {
+            setContentView(binding.root)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+
+        binding.tvCamera.setOnClickListener {
+
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        // Here after all the permission are granted launch the CAMERA to capture an image.
+                        if (report?.areAllPermissionsGranted() == true) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+
+            // TODO Step 7: Ask for the permission while selecting the image from Gallery using Dexter Library. And Remove the toast message.
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                        // Here after all the permission are granted launch the gallery to select and image.
+                        //            if (report?.areAllPermissionsGranted() == true) {
+                        // TODO Step 8: Show the Toast message for now just to know that we have the permission.
+                        // START
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                        // END
+                        //}
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+            // END
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.extras?.let {
+                    val thumbnail: Bitmap =
+                        data.extras!!.get("data") as Bitmap
+                    mBinding.ivDishImage.setImageBitmap(thumbnail)
+                    mBinding.ivAddDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this,
+                            R.drawable.ic_vector_edit
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("Go To Settings")
+            { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }.show()
     }
 }
